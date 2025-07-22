cc4be71a3a45a757a81dcb9d7fbeaf9f4cec6ee3
Aaron-1351 <bergaaron29@gmail.com>
2025-07-23 01:07:14 +1000

    Main Menu
    This commit adds in the Main menu with both the quit and start button that is able to take you to the first level scene. Right now it just takes you to what will be the intro scene as a test but will be fixed once the first level is made.

---
+++ Assets/Scripts/Scene Buttons.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneButtons : MonoBehaviour
+{
+    public void playButton()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
